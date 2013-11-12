306450f66b0661dc08877c647a428ce6a468bf69
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-11-12 09:04:16 -0600

    added some erb to example file

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,13 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = 3
+z = 5
+
+answer1 = ERB.new "The value of <%= y %> times <%= z %>  is: <%= y * z %>"
+puts answer1.result(binding)
+
+h = {:book1 =>"The Old Man & the Sea", :book2 => "The Also Rises"}
+hash1 = ERB.new "Some of my favorite Hemingway books are: <%= h.each_value {|value| puts value} %>"
+puts hash1.result(binding)
